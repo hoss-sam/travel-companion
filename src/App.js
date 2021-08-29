2bdb51eff7b9f3c143ebb83fe0b312cc9109b422
Hossam Ali <hossam.alawadly@gmail.com>
2021-08-30 00:12:36 +0200

    Get resturants and render side list based on location

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { getPlacesData } from './api';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+
+const App = () => {
+    
+    const [places, setPlaces] = useState([]);
+    const [coords, setCoords] = useState({});
+    const [bounds, setBounds] = useState({});
+
+    useEffect(()=> {
+        navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude} })=> {
+            setCoords({ lat: latitude, lng: longitude});
+        })
+    })
+
+    useEffect(()=> {
+        getPlacesData(bounds.sw, bounds.ne)
+            .then((data)=> {
+                console.log(data)
+                setPlaces(data);
+            })
+    }, [coords, bounds]);
+    
+    return (
+        <>
+            <CssBaseline /> 
+            <Header />
+            <Grid container spacing={3} style={{width: '100%'}}>
+                <Grid item xs={12} md={4}>
+                    <List places={places} />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map 
+                        coords={coords}
+                        setCoords={setCoords}
+                        setBounds={setBounds}
+                        places={places}  
+                    />
+                </Grid>
+            </Grid>
+        </>
+    );
 }
 
 export default App;
